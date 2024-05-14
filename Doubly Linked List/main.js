020e5c533c524337ed3ae88608aa82da173f6824
Sherzod Axadov <sherzodaxadov69@gmail.com>
2024-05-14 14:37:53 +0500

    doubly linked list: set, insert, remove

--- Doubly Linked List/main.js
+++ Doubly Linked List/main.js
@@ -96,6 +96,50 @@ class DoublyLinkedList {
 
         return temp
     }
+
+    set(index, value) {
+        let temp = this.get(index)
+        if (temp) {
+            temp.value = value
+        }
+
+        return temp
+    }
+
+    insert(index, value) {
+        if (index === 0) return this.unshift(value)
+        else if (index === this.length) return this.push(value)
+        else if (index < 0 || index > this.length) return false
+
+        const newNode = new Node(value)
+        const before = this.get(index - 1)
+        const after = before.next
+
+        before.next = newNode
+        newNode.prev = before
+
+        newNode.next = after
+        after.prev = newNode
+
+        this.length++
+        return this
+    }
+
+    remove(index) {
+        if (index === 0) return this.shift()
+        if (index === this.length - 1) return this.pop()
+        if (index < 0 || index >= this.length) return false
+
+        let temp = this.get(index)
+
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        temp.next = null
+        temp.prev = null
+
+        this.length--
+        return temp
+    }
 }
 
 let dll = new DoublyLinkedList(10)
@@ -104,3 +148,9 @@ dll.push(30)
 
 // console.log(dll)
 // console.log(dll.pop())
+// console.log(dll.set(3, 40))
+// console.log(dll.set(2, 40))
+
+// console.log(dll.insert(2, 25))
+
+console.log(dll.remove(1))
