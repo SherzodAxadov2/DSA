48b179a1fd9505689b8267363080c6fe7ff67511
Sherzod Axadov <sherzodaxadov69@gmail.com>
2024-05-30 18:08:44 +0500

    tree: contains, minValue, minNodeValue, BFS methods

--- Trees/main.js
+++ Trees/main.js
@@ -23,7 +23,7 @@ class BST {
         while (true) {
             if (newNode.value === temp.value) return undefined
             if (newNode.value < temp.value) {
-                if (!temp.left) {  // when left node is null then set newNode
+                if (!temp.left) {
                     temp.left = newNode
                     return this
                 }
@@ -37,6 +37,57 @@ class BST {
             }
         }
     }
+
+    contains(value) {
+        if (!this.root) return false
+        let temp = this.root
+
+        while (temp) {
+            if (temp.value === value) return true
+            else if (value < temp.value) {
+                temp = temp.left
+            } else {
+                temp = temp.right
+            }
+        }
+
+        return false
+    }
+
+    minValue() {
+        if (!this.root) return undefined
+
+        let temp = this.root
+        while (temp.left) {
+            temp = temp.left
+        }
+
+        return temp.value
+    }
+
+    minValueNode(currentNode) {
+        while (currentNode.left) {
+            currentNode = currentNode.left
+        }
+
+        return currentNode.value
+    }
+
+    BFS() {
+        let currentNode = this.root
+        let queue = []
+        let results = []
+        queue.push(currentNode)
+
+        while (queue.length) {
+            currentNode = queue.shift()
+            results.push(currentNode.value)
+            if (currentNode.left) queue.push(currentNode.left)
+            if (currentNode.right) queue.push(currentNode.right)
+        }
+
+        return results
+    }
 }
 
 let myTree = new BST()
@@ -48,3 +99,8 @@ myTree.insert(18)
 
 myTree.insert(52)
 myTree.insert(82)
+
+// console.log(myTree.contains(18))
+// console.log(myTree.minValue())
+// console.log(myTree.minValueNode(myTree.root.right))
+console.log(myTree.BFS())
