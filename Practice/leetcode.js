2b21954e2905ef768612899348b858946230b8fa
SherzodAxadov2 <sherzodaxadov69@gmail.com>
2024-07-08 15:16:50 +0500

    leetcode: nextGreater element

--- Practice/leetcode.js
+++ Practice/leetcode.js
@@ -1513,3 +1513,21 @@ const countOdds = function (low, high) {
         return Math.floor((high - low) / 2) + 1
     }
 };
+
+const nextGreaterElement = function(nums1, nums2) {
+    const map = new Map();
+    const stack = [];
+    for (let num of nums2) {
+        while (stack.length && stack[stack.length - 1] < num) {
+            map.set(stack.pop(), num);
+        }
+        stack.push(num);
+    }
+
+    for (let i = 0; i < nums1.length; i++) {
+        nums1[i] = map.has(nums1[i]) ? map.get(nums1[i]) : -1;
+    }
+    return nums1;
+}
+
+console.log(nextGreaterElement([4,1,2], [1,3,4,2]))
